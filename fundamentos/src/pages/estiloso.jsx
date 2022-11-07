85a600e6899add31da0b1c3a98d892ea625248a2
NikolasDaltro <nikolasdsantos@gmail.com>
2022-11-07 17:09:09 -0300

    Componente Layout

--- fundamentos/src/pages/estiloso.jsx
+++ fundamentos/src/pages/estiloso.jsx
@@ -1,13 +1,13 @@
 import style from '../styles/Estiloso.module.css'
-import Link from 'next/link'
+import Layout from '../components/Layout'
+
 
 export default function Stylo(){
     return(
+        <Layout titulo="Exemplo de CSS Modularizado">
         <div className={style.roxo}>
-            <Link href="/">
-            Voltar
-            </Link>
-            <h1>Estilo usando Css Módulos</h1>
+         <h1>Estilo usando Css Módulos</h1>
         </div>
+        </Layout>
     )
 }
