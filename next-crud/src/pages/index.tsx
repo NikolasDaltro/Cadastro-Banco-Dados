80370cc12f773f77178481f83547d9ea6d2a0009
NikolasDaltro <nikolasdsantos@gmail.com>
2022-11-10 17:00:04 -0300

    Part #03 tabela, Finalizando Layout da tabela

--- next-crud/src/pages/index.tsx
+++ next-crud/src/pages/index.tsx
@@ -11,6 +11,15 @@ export default function Home() {
     new Cliente('Brenda' , 44, '4'),
   ]
 
+  function clienteSelecionado(cliente: Cliente){
+    console.log(cliente.nome)
+
+  }
+  function clienteExcluido(cliente: Cliente){
+    console.log(`Excluir...${cliente.nome}`)
+
+  }
+
   return (
     <div className={`
     flex justify-center items-center h-screen
@@ -18,7 +27,8 @@ export default function Home() {
      text-white
     `}>
      <Layout titulo="Cadastro Simples">
-        <Tabela clientes={clientes}></Tabela>
+        <Tabela clientes={clientes}  clienteSelecionado={clienteSelecionado}
+        clienteExcluido={clienteExcluido}></Tabela>
      </Layout>
     </div>
   )
